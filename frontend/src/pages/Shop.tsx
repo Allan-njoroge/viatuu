61a64c8daef217f624917b02e9c6a36cc4c6cfa2
Allan-njoroge <allandavis254@gmail.com>
2025-01-08 14:53:09 +0300

    Shop page basic UI done

--- frontend/src/pages/Shop.tsx
+++ frontend/src/pages/Shop.tsx
@@ -1,7 +1,17 @@
+import Filters from "@/components/Filters";
+import Products from "@/section/Shop/Products";
+import Footer from "@/components/Footer";
+
 const Shop = () => {
   return (
-    <div>Shop</div>
-  )
-}
+    <>
+      <Filters />
+      <div className="max-w-[1440px] w-full mx-auto pb-20 border-b border-b-muted-foreground/30">
+        <Products />
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Shop
+export default Shop;
