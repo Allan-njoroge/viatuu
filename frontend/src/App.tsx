2cbbf1beeb8683f94e8951cf2d16f39115190492
Allan-njoroge <allandavis254@gmail.com>
2025-01-10 16:26:59 +0300

    :)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import ProductPage from "./pages/ProductPage";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Auth from "./pages/Auth";
+
+import Navbar from "./components/Navbar";
+
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={ <Auth /> } />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/shop/:category" element={ <Shop /> } />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
